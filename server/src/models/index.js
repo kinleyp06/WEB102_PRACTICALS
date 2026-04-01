959240e2d7482505b7a465180203faf535d47930
kinleyp06 <kinleypem06@gmail.com>
2026-04-01 09:53:17 +0600

    practical 2

---
+++ server/src/models/index.js
@@ -0,0 +1,15 @@
+let users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Alice" }
+];
+
+let videos = [
+  { id: 1, title: "Video 1", userId: 1 },
+  { id: 2, title: "Video 2", userId: 2 }
+];
+
+let comments = [
+  { id: 1, text: "Nice!", videoId: 1 }
+];
+
+module.exports = { users, videos, comments };
